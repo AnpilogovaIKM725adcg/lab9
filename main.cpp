0338f3b1210f070bc9cbfb3e69c3039451714e12
AnpilogovaIKM725adcg <Alisa.Anpilohova@infiz.khpi.edu.ua>
2026-03-23 18:21:29 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int ROWS = 5;
+    const int COLS = 5;
+    double array[ROWS][COLS];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            array[i][j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            cout << array[i][j] << '\t';
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    double max = array[0][0];
+    int row = 0;
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (array[i][j] > max)
+            {
+                max = array[i][j];
+                row = i;
+            }
+        }
+    }
+
+    double sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (j % 2 == 1)
+            {
+                sum += array[i][j];
+                count++;
+            }
+        }
+    }
+
+    double average = sum / count;
+
+    cout << "Максимальний елемент: " << max << endl;
+    cout << "Номер рядка: " << row + 1 << endl;
+    cout << "Середнє арифметичне тільки в парних стовпців: " << average << endl;
+}
